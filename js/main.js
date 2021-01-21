16c56a35d9cdb512b2a58b6c90ff0f6888bb968c
Cullen <cullenyanderson@gmail.com>
2021-01-21 16:28:56 -0500

    minor fixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// fits home page to device's screen
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+// makes navbar collapse when link is pressed
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
